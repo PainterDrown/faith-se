706fa40281b5a3602fe71412458b53577b02fc35
PainterDrown <painterdrown@hotmail.com>
2017-08-16 11:50:14 +0800

    新增数据库表设计

--- src/services/db/index.js
+++ src/services/db/index.js
@@ -1,10 +1,24 @@
 const mysql = require('mysql');
+const { promisifyAll, FaithError } = require('../../utils');
 const { mysql: mysql_options } = require('../../config');
+
+promisifyAll(require('mysql/lib/Connection').prototype);
+promisifyAll(require('mysql/lib/Pool').prototype);
 
 const pool = mysql.createPool(mysql_options);
 
+/**
+ * 查询结果永远是[Object]
+ * @param  {String}   sql    sql语句
+ * @param  {[String]} values sql参数
+ * @return {[Object]}        查询结果
+ */
 async function queryDB(sql, values) {
-
+  try {
+    return pool.queryAsync(sql, values);
+  } catch (err) {
+    throw new FaithError(500, '数据库查询错误', err);
+  }
 };
 
 exports = module.exports = queryDB;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,11 @@
-const logger = require('./logger');
+const logger       = require('./logger');
 const toMiddleware = require('./toMiddleware');
+const promisifyAll = require('./promisifyAll');
+const FaithError   = require('./FaithError');
 
 exports = module.exports = {
   logger,
   toMiddleware,
+  promisifyAll,
+  FaithError,
 };
